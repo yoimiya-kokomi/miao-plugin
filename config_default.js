c8a0887c66aaa087d651a6bc0de23711ba73c286
yoimiya-kokomi <592981798@qq.com>
2022-04-16 17:41:51 +0800

    调整config_default.js描述

--- config_default.js
+++ config_default.js
@@ -1,4 +1,9 @@
-export default {
+/*
+* 请将此文件复制一份，命名为 config.js 放置在miao-plugin的根目录
+*
+* */
+
+export const config = {
   miaoApi: {
     api: "http://49.232.91.210:88/miaoPlugin/GetRoleAll", // Miao APi 地址
     qq: "", // 申请的主人qq
