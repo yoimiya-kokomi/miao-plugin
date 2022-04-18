d8c039d6ca4477b07065db27f7efd2c0ebbaa684
yoimiya-kokomi <592981798@qq.com>
2022-04-19 05:32:21 +0800

    深渊出场率及角色持有率改为请求MiaoPlugin的中间服务转发

--- components/models/HutaoApi.js
+++ components/models/HutaoApi.js
@@ -8,44 +8,27 @@
 import Base from "./Base.js";
 import fetch from "node-fetch";
 
-const host = "https://hutao-api.snapgenshin.com";
+const host = "http://49.232.91.210:88/miaoPlugin/hutaoApi";
 
 let hutaoApi = null;
 
 function getApi(api) {
-  return `${host}${api}`;
+  return `${host}?api=${api}`;
 }
 
 let HutaoApi = {
-  _auth: false,
-  async login() {
-    let response = await fetch(getApi('/Auth/Login'), {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/json; charset=utf-8',
-      },
-      body: JSON.stringify({
-        "Appid": "appid",
-        "Secret": "secret"
-      })
-    });
-    let res = await response.json();
-  },
   async req(url, data) {
     let cacheData = await redis.get(`hutao:${url}`);
     if (cacheData) {
       return JSON.parse(cacheData)
     }
 
-    let response = await fetch(getApi(`${url}?Authorization=demo`), {
+    let response = await fetch(getApi(`${url}`), {
       method: "GET",
-      headers: {
-        Authorization: `Bearer{token}`
-      }
     });
     let retData = await response.json();
     let d = new Date();
-    retData.lastUpdate = `${d.toLocaleDateString()} ${d.toTimeString().substr(0, 5)}`;
+     retData.lastUpdate = `${d.toLocaleDateString()} ${d.toTimeString().substr(0, 5)}`;
     await redis.set(`hutao:${url}`, JSON.stringify(retData), { EX: 3600 });
     return retData;
 
--- config_default.js
+++ config_default.js
@@ -1,12 +1,12 @@
 /*
 * 请将此文件复制一份，命名为 config.js 放置在miao-plugin的根目录
-*
+* 申请API Token请在Bot群内联系 @喵喵 或者 @九章
 * */
 
 export const config = {
   miaoApi: {
-    api: "http://49.232.91.210:88/miaoPlugin/GetRoleAll", // Miao APi 地址
+    api: "http://49.232.91.210:88/miaoPlugin/getRoleAll", // Miao APi 地址
     qq: "", // 申请的主人qq
-    token: "" // 申请的token
+    token: "" // 申请的token，申请API Token请在Bot群内联系 @喵喵 或者 @九章
   }
 }
