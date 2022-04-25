f0f4df9bc6c113d6e92bc6f25c6ede031fe00f1b
yoimiya-kokomi <592981798@qq.com>
2022-04-26 04:14:30 +0800

    #角色详情 伤害计算增加 八重神子

---
+++ resources/meta/character/八重神子/calc.js
@@ -0,0 +1,37 @@
+export const details = [{
+  check: ({ cons }) => cons < 2,
+  title: "叄阶杀生樱伤害",
+  dmg: ({ talent, attr }, dmg) => dmg(talent.e["杀生樱伤害·叁阶"], "e")
+}, {
+  check: ({ cons }) => cons >= 2,
+  title: "肆阶杀生樱伤害",
+  dmg: ({ talent, attr }, dmg) => dmg(talent.e["杀生樱伤害·肆阶"], "e")
+}, {
+  title: "Q天狐霆雷伤害",
+  dmg: ({ talent }, dmg) => dmg(talent.q['天狐霆雷伤害'], "q")
+}, {
+  title: "四段Q总伤害",
+  dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'] + talent.q['天狐霆雷伤害'] * 3, 'q')
+}];
+
+export const buffs = [{
+  title: `被动天赋：基于元素精通提高杀生樱伤害[eDmg]%`,
+  data: {
+    "eDmg": ({ attr, calc }) => {
+      console.log(attr.mastery, calc(attr.mastery))
+      return calc(attr.mastery) * 0.15
+    }
+  }
+}, {
+  check: ({ cons }) => cons >= 4,
+  title: "4命效果：杀生樱命中敌人后提高雷伤[dmg]%",
+  data: {
+    dmg: 20
+  }
+}, {
+  cons: 6,
+  title: "6命效果：杀生樱无视敌人[eDef]%防御",
+  data: {
+    eDef: 60
+  }
+}];
---
+++ resources/meta/character/甘雨/calc.js
@@ -0,0 +1,18 @@
+export const details = [];
+
+export const defParams = {
+
+};
+export const buffs = [{
+  cons: 1,
+  title: "1命效果：霜华失命中减少敌人15%冰抗",
+  data: {
+    kx: 15
+  }
+}, {
+  cons: 4,
+  title: "4命效果：大招领域内敌人受到的伤害提升25%",
+  data: {
+    dmg: 25
+  }
+}];
