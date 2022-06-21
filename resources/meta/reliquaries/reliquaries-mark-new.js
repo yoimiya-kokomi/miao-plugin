586ad4b23b410c42ab10c9db041b804744248a32
yoimiya-kokomi <592981798@qq.com>
2022-06-22 06:15:53 +0800

    补充久岐忍圣遗物评分中精通的权重设置

--- resources/meta/reliquaries/reliquaries-mark-new.js
+++ resources/meta/reliquaries/reliquaries-mark-new.js
@@ -94,5 +94,5 @@ export const usefulAttr = {
   '砂糖': { hp: 0, atk: 75, def: 0, cp: 100, cd: 100, mastery: 100, dmg: 40, phy: 0, recharge: 70, heal: 0 },
   '雷泽': { hp: 0, atk: 75, def: 0, cp: 100, cd: 100, mastery: 0, dmg: 50, phy: 100, recharge: 0, heal: 0 },
   '夜兰': { hp: 80, atk: 0, def: 0, cp: 100, cd: 100, mastery: 0, dmg: 100, phy: 0, recharge: 75, heal: 0 },
-  '久岐忍': { hp: 100, atk: 0, def: 0, cp: 80, cd: 80, mastery: 0, dmg: 100, phy: 0, recharge: 75, heal: 100 },
+  '久岐忍': { hp: 100, atk: 0, def: 0, cp: 80, cd: 80, mastery: 80, dmg: 100, phy: 0, recharge: 75, heal: 100 },
 };
