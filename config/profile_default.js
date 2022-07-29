36064596f4a856dd216c4dc545a1ddee6ea0163b
yoimiya-kokomi <592981798@qq.com>
2022-07-30 02:54:27 +0800

    从profile_default.js中去除UA设置，防止被勿更改 UA移至系统设置中

--- config/profile_default.js
+++ config/profile_default.js
@@ -6,14 +6,22 @@
 
 /*
 * Enka面板服务API配置
-* *
-* Enka官网：https://enka.shinshin.moe/
-* *
-* 其余API待确认后添加
+*
+* 【Enka官网】：https://enka.shinshin.moe/
+* 感谢Enka提供的面板查询服务
+* https://github.com/yoimiya-kokomi/miao-plugin/issues/63#issuecomment-1199348789
+* 如果可以的话，也可考虑在 Patreon上支持Enka，或提供不用的原神账户，具体可在Discord联系
+*
+*
+* 如Enka服务访问不稳定，可尝试更换MiniGrayGay大佬提供的中转服务
+* 【链接1】：https://enka.microgg.cn/
+* 【链接2】：https://enka.minigg.cn/
+* 感谢@MiniGrayGay 大佬提供的服务(Github: https://github.com/MiniGrayGay)
+*
 * */
+
 export const enkaApi = {
-  url: 'https://enka.shinshin.moe/',
-  userAgent: 'Miao-Plugin/3.0'
+  url: 'https://enka.shinshin.moe/'
 }
 
 /*
