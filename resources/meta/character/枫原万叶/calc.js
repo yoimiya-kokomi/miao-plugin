063b27a294ef8d02f730f15289afb27071872cb4
yoimiya-kokomi <592981798@qq.com>
2022-08-07 03:06:12 +0800

    调整character的side.png及face.png

--- resources/meta/character/枫原万叶/calc.js
+++ resources/meta/character/枫原万叶/calc.js
@@ -1,26 +1,27 @@
 export const details = [{
-  title: "E长按伤害",
+  title: 'E长按伤害',
   dmg: ({ talent }, dmg) => dmg(talent.e['长按技能伤害'], 'e')
 }, {
-  title: "Q斩击伤害",
+  title: 'Q斩击伤害',
   dmg: ({ talent }, dmg) => dmg(talent.q['斩击伤害'], 'q')
 }, {
-  title: "Q无转化每段伤害",
+  title: 'Q无转化每段伤害',
   dmg: ({ talent }, dmg) => dmg(talent.q['持续伤害'], 'q')
 }, {
-  title: "扩散反应伤害",
+  title: '扩散反应伤害',
   dmg: ({}, { ks }) => ks()
-}];
+}]
 
-export const mainAttr = "atk,cpct,cdmg,mastery";
+export const mainAttr = 'atk,cpct,cdmg,mastery'
 
 export const buffs = [{
-  title: "万叶2命：开Q后精通提高200",
+  title: '万叶2命：开Q后精通提高200',
+  cons: 2,
   data: {
     mastery: 200
   }
 }, {
-  title: "元素精通：扩散伤害提高[ks]%",
+  title: '元素精通：扩散伤害提高[ks]%',
   sort: 2,
-  mastery: "ks"
-}];
+  mastery: 'ks'
+}]
--- resources/meta/character/烟绯/calc.js
+++ resources/meta/character/烟绯/calc.js
@@ -1,34 +1,34 @@
 export const details = [{
-  title: "开Q满丹火印重击",
+  title: '开Q满丹火印重击',
   params: { dhy: 15 },
   dmg: ({ talent, cons }, dmg) => dmg(talent.a['重击伤害2'][cons * 1 === 6 ? 4 : 3], 'a2')
 }, {
-  title: "开Q满丹火印重击蒸发",
+  title: '开Q满丹火印重击蒸发',
   params: { dhy: 15 },
   dmg: ({ talent, cons }, dmg) => dmg(talent.a['重击伤害2'][cons * 1 === 6 ? 4 : 3], 'a2', 'zf')
 }, {
-  title: "E伤害",
+  title: 'E伤害',
   dmg: ({ talent }, dmg) => dmg(talent.e['技能伤害'], 'e')
-}];
+}]
 
 export const buffs = [{
-  title: "烟绯被动：重击消耗4枚丹火印增加20%火伤",
+  title: '烟绯被动：重击消耗4枚丹火印增加20%火伤',
   cons: 6,
   data: {
     a2Dmg: ({ params }) => params.dhy ? 20 : 0
   }
 }, {
-  title: "烟绯被动：重击消耗3枚丹火印增加15%火伤",
+  title: '烟绯被动：重击消耗3枚丹火印增加15%火伤',
   check: ({ cons }) => cons < 6,
   data: {
     a2Dmg: ({ params }) => params.dhy ? 15 : 0
   }
 }, {
-  title: "烟绯被动：开Q后提高重击伤害[a2Dmg]%",
+  title: '烟绯被动：开Q后提高重击伤害[a2Dmg]%',
   data: {
     a2Dmg: ({ talent }) => talent.q['重击伤害额外加成']
   }
 }, {
-  title: "元素精通：蒸发融化伤害提高[zf]%",
-  mastery: "zf,rh"
+  title: '元素精通：蒸发融化伤害提高[zf]%',
+  mastery: 'zf,rh'
 }]
