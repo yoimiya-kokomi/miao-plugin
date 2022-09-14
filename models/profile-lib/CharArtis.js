aa866cb52bee00127cfcf6297366271db4129d17
Panganqi_vrlab <pangaq@shanghaitech.edu.cn>
2022-09-14 17:04:42 +0800

    add weapon and weapon refine weight

--- models/profile-lib/CharArtis.js
+++ models/profile-lib/CharArtis.js
@@ -32,11 +32,25 @@ const CharArtis = {
 
     let def = function (attrWeight) {
       let title = '通用'
-      let weight = attrWeight || usefulAttr[char.name] || { atk: 75, cp: 100, cd: 100 }
+      let weight = attrWeight || usefulAttr[char.name] || { atk: 75, cp: 100, cd: 100, def: 0 }
+      if (profile.weapon.name==='磐岩结绿'){
+        weight.hp = weight.hp + 10 + 3 * profile.weapon.affix
+        title = '绿剑'
+      }
+      if (profile.weapon.name==='护摩之杖'){
+        weight.hp = weight.hp + 10 + 3 * profile.weapon.affix
+        title = '护膜'
+      }
+      // 判定薙刀精炼收益，需要考虑本身充能是否已经足够高收益以及是否为云堇（通过防御力收益）
+      if (profile.weapon.name==='薙草之稻光' && weight.recharge < 65){
+        weight.recharge = weight.recharge + 10 + 3 * profile.weapon.affix
+        title = '薙刀'
+      }
       if (artis.is('绝缘4') && weight.recharge < 75) {
-        weight.recharge += 25
+        weight.recharge = 75
         title = '绝缘4'
       }
+      
       return {
         title: `${char.abbr}-${title}`,
         attrWeight: weight
