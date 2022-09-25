90c608181102949247deca1fc1f6b33797114ac2
yoimiya-kokomi <592981798@qq.com>
2022-09-25 22:04:02 +0800

    `#雷神面板`命令提示中角色名丢失的bug fix

--- apps/character/profile-detail.js
+++ apps/character/profile-detail.js
@@ -88,7 +88,7 @@ export async function renderProfile (e, char, mode = 'profile', params = {}) {
   return await Common.render('character/profile-detail', {
     save_id: uid,
     uid,
-    data: avatar.getData('name,cons,level,weapon,talent,dataSource,updateTime'),
+    data: avatar.getData('name,abbr,cons,level,weapon,talent,dataSource,updateTime'),
     attr,
     elem: char.elem,
     dmgData,
