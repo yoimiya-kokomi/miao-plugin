55b31f73c5f43bb419c006a3c2aa120670d4392c
Kokomi <102026640+yoimiya-kokomi@users.noreply.github.com>
2022-11-19 20:54:11 +0800

    增加莱依拉的伤害计算及圣遗物评分权重

---
+++ resources/meta/character/坎蒂丝/calc.js
@@ -0,0 +1,31 @@
+export const details = [{
+  title: 'E护盾吸收',
+  dmg: ({ attr, calc, talent }, { shield }) =>
+    shield(calc(attr.hp) * talent.e['护盾吸收量2'][0] / 100 + talent.e['护盾吸收量2'][1])
+}, {
+  title: '蓄力E伤害',
+  dmg: ({ calc, attr, talent }, { basic }) => basic(calc(attr.hp) * talent.e['蓄力完成伤害'] / 100)
+}, {
+  title: 'Q提升普攻伤害',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: 20 + Math.floor(calc(attr.hp) / 1000) * 0.5,
+      unit: '%'
+    }
+  }
+}]
+
+export const mainAttr = 'hp,atk,cpct,cdmg'
+
+export const buffs = [{
+  title: '坎蒂丝2命：E命中敌人提升20%生命值',
+  cons: 2,
+  data: {
+    hpPct: 20
+  }
+}, {
+  title: '坎蒂丝被动：Q伤害加成基于生命值上限提升[_a]%',
+  data: {
+    _a: ({ attr, calc }) => Math.floor(calc(attr.hp) / 1000) * 0.5
+  }
+}]
---
+++ resources/meta/character/莱依拉/calc.js
@@ -0,0 +1,38 @@
+export const details = [{
+  title: '满层E护盾量',
+  dmg: ({ attr, calc, talent }, { shield }) =>
+    shield(calc(attr.hp) * talent.e['护盾基础吸收量2'][0] / 100 + talent.e['护盾基础吸收量2'][1])
+}, {
+  title: 'E飞星单伤害',
+  dmg: ({ attr, talent }, dmg) => dmg(talent.e['飞星伤害'], 'e')
+}, {
+  title: 'Q星光弹伤害',
+  dmg: ({ attr, calc, talent }, { basic }) => basic(calc(attr.hp) * talent.q['星光弹伤害'] / 100)
+}]
+
+export const mainAttr = 'hp,atk,cpct,cdmg'
+
+export const buffs = [{
+  title: '莱依拉1命：安眠帷幕护盾的伤害吸收量提高20%',
+  cons: 1,
+  data: {
+    shield: 20
+  }
+}, {
+  title: '莱依拉6命：飞星与星光弹造成的伤害提升40%',
+  cons: 6,
+  data: {
+    eDmg: 40,
+    qDmg: 40
+  }
+}, {
+  title: '莱依拉被动：满层护盾强效提升24%',
+  data: {
+    shield: 24
+  }
+}, {
+  title: '莱依拉被动：飞星造成的伤害值基于生命值提高[ePlus]',
+  data: {
+    ePlus: ({ attr, calc }) => calc(attr.hp) * 0.015
+  }
+}]
