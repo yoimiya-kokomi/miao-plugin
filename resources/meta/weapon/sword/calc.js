a69de3f9683cca14e54a4e37d952216ccea8e78d
panganqi <pangaq@shanghaitech.edu.cn>
2023-01-21 13:06:42 +0800

    更新海哥专武计算并增加伤害计算对其支持 (#414)

--- resources/meta/weapon/sword/calc.js
+++ resources/meta/weapon/sword/calc.js
@@ -151,8 +151,8 @@ export default function (step, staticStep) {
     裁叶萃光: [staticStep('cpct', 4), {
       title: '普攻与元素战技造成的伤害值提高[aPlus]',
       data: {
-        aPlus: ({ attr, calc, refine }) => calc(attr.mastery) * [200, 230, 260, 290, 320][refine] / 100,
-        ePlus: ({ attr, calc, refine }) => calc(attr.mastery) * [200, 230, 260, 290, 320][refine] / 100
+        aPlus: ({ attr, calc, refine }) => calc(attr.mastery) * step(120)[refine] / 100,
+        ePlus: ({ attr, calc, refine }) => calc(attr.mastery) * step(120)[refine] / 100
       }
     }],
     斫峰之刃: [{
