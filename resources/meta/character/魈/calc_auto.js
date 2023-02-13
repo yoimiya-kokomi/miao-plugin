d86466c9caec82b5c9b32d283890a76b1bb35939
panganqi <pangaq@shanghaitech.edu.cn>
2023-02-13 23:12:23 +0800

    Create calc_auto.js

---
+++ resources/meta/character/魈/calc_auto.js
@@ -0,0 +1,63 @@
+export const details = [{
+  title: '风轮两立',
+  params: { team:false},
+  dmg: ({ talent }, dmg) => dmg(talent.e['技能伤害'], 'e')
+}, {
+  title: '开Q满被动E',
+  params: { e: 1, layer: 5 ,team:false},
+  dmg: ({ talent }, dmg) => dmg(talent.e['技能伤害'], 'e')
+}, {
+  title: '靖妖傩舞·首插',
+  params: {
+    layer: 1 , team:false
+  },
+  dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][1], 'a3')
+}, {
+  title: '靖妖傩舞·尾插',
+  params: {
+    layer: 5 , team:false
+  },
+  dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][1], 'a3')
+}, {
+  title: '魈珐班 开Q满被动E',
+  params: { e: 1, layer: 5 ,team:true},
+  dmg: ({ talent }, dmg) => dmg(talent.e['技能伤害'], 'e')
+}]
+
+export const defDmgIdx = 4
+export const mainAttr = 'atk,cpct,cdmg'
+
+export const defParams = {
+  layer: 0,
+  team:true
+}
+
+export const buffs = [{
+  title: '靖妖傩舞：下落攻击伤害提升[a3Dmg]%',
+  data: {
+    a3Dmg: ({ talent }) => talent.q['普通攻击/重击/下落攻击伤害提升']
+  }
+}, {
+  title: '魈天赋：开Q后每3秒伤害提升5%，满层提高25%',
+  data: {
+    dmg: ({ params }) => params.layer * 5
+  }
+}, {
+  title: '魈被动：3层E使E的伤害提高45%',
+  data: {
+    eDmg: ({ params }) => params.e ? 45 : 0
+  }
+}, {check: ({ params }) => params.team === true,
+    title: '风鹰宗室班：增加[atkPlus]点攻击力与[atkPct]%攻击力',
+    data: {
+      atkPct: 20,
+      atkPlus: 1202.35
+  }
+  }, {check: ({ params }) => params.team === true,
+    title: '满命珐露珊：获得[dmg]%增伤,获得[kx]%减抗,获得[cdmg]%爆伤',
+    data: {
+      dmg: 40,
+      kx: 30,
+      cdmg:40,
+   }
+  }]
