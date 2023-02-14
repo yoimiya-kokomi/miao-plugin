66845d61c10f187d7be5087f90dd46391231749b
panganqi <pangaq@shanghaitech.edu.cn>
2023-02-14 10:36:07 +0800

    添加草神，魈，散兵组队伤害 (#466)

---
+++ resources/meta/character/纳西妲/calc_auto.js
@@ -0,0 +1,106 @@
+export const details = [{
+  title: 'E长按伤害',
+  params: { e: true, q: false ,team : false},
+  dmg: ({ talent }, dmg) => dmg(talent.e['长按伤害'], 'e')
+}, {
+  title: '灭净三业伤害',
+  params: { e2: true, q: false ,team : false},
+  dmg: ({ talent, calc, attr }, { basic }) => {
+    const td = talent.e['灭净三业伤害2']
+    const em = calc(attr.mastery)
+    const atk = calc(attr.atk)
+    return basic(td[0] * atk / 100 + td[1] * em / 100, 'e')
+  }
+}, {
+  title: '开Q灭净三业伤害',
+  params: { e2: true ,team : false},
+  dmg: ({ talent, calc, attr, cons }, { basic }) => {
+    const td = talent.e['灭净三业伤害2']
+    const em = calc(attr.mastery)
+    const atk = calc(attr.atk)
+    return basic(td[0] * atk / 100 + td[1] * em / 100, 'e')
+  }
+}, {
+  title: '灭净三业·蔓激化',
+  params: { e2: true, q: false ,team : false},
+  dmg: ({ talent, calc, attr }, { basic }) => {
+    const td = talent.e['灭净三业伤害2']
+    const em = calc(attr.mastery)
+    const atk = calc(attr.atk)
+    return basic(td[0] * atk / 100 + td[1] * em / 100, 'e', 'spread')
+  }
+}, {
+  title: '开Q灭净三业·蔓激化',
+  params: { e2: true ,team : false},
+  dmg: ({ talent, calc, attr, cons }, { basic }) => {
+    const td = talent.e['灭净三业伤害2']
+    const em = calc(attr.mastery)
+    const atk = calc(attr.atk)
+    return basic(td[0] * atk / 100 + td[1] * em / 100, 'e', 'spread')
+  }
+}, {
+  title: '提八纳皇开Q灭净三业·蔓激化',
+  params: { e2: true ,team : true},
+  dmg: ({ talent, calc, attr, cons }, { basic }) => {
+    const td = talent.e['灭净三业伤害2']
+    const em = calc(attr.mastery)
+    const atk = calc(attr.atk)
+    return basic(td[0] * atk / 100 + td[1] * em / 100, 'e', 'spread')
+  }
+}]
+
+
+export const defParams = {
+    q: true,team:true
+}
+
+export const defDmgIdx = 5
+export const mainAttr = 'atk,mastery,cpct,cdmg'
+
+export const buffs = [{
+  title: '草神1命：火元素队友额外计入1位',
+  cons: 1
+}, {
+  title: '草神2命：激化相关反应降低敌人防御力30%',
+  cons: 2,
+  data: {
+    enemyDef: 30
+  }
+}, {
+  title: '草神4命：E4个敌人提升精通160',
+  cons: 4,
+  data: {
+    mastery: 160
+  }
+}, {
+  check: ({ cons,params }) =>  (cons <=4 && params.team === true),
+  title: '提八皇组队buff：双草教官精1终末皇女[mastery],[atkPct]%攻击力',
+  data: {
+    mastery:320,
+    atkPct:20
+  }
+},{
+  check: ({ cons,params }) =>  (cons >4 && params.team === true),
+  title: '提八皇组队buff：双草教官精5终末皇女4命提纳里[mastery],[atkPct]%攻击力',
+  data: {
+    mastery:540,
+    atkPct:40
+  }
+}, {
+  title: '草神被动：开Q元素精通提升[mastery]',
+  data: {
+    mastery: ({ calc, attr, params }) => (params.q === false ? 0 : 1) * Math.min(250, calc(attr.mastery) * 0.25)
+  }
+}, {
+  title: '草神被动：基于元素精通提升灭净三业伤害[eDmg]%，暴击率[eCpct]%',
+  data: {
+    eDmg: ({ calc, attr, params }) => (params.e ? 0 : 1) * Math.min(80, (calc(attr.mastery) - 200) * 0.1),
+    eCpct: ({ calc, attr, params }) => (params.e ? 0 : 1) * Math.min(24, (calc(attr.mastery) - 200) * 0.03)
+  }
+}, {
+  title: '草神Q：开Q提升灭净三业伤害[eDmg]%',
+  data: {
+    eDmg: ({ cons, talent, params }) => (params.q === false ? 0 : 1) *
+      (cons >= 1 ? talent.q['火2伤害提升'] : talent.q['火1伤害提升'])
+  }
+}]
