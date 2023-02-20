072b0a864338acddb8fa87dc3eb430d289148480
panganqi <pangaq@shanghaitech.edu.cn>
2023-02-20 20:53:30 +0800

    Update artis.js

--- resources/meta/character/迪希雅/artis.js
+++ resources/meta/character/迪希雅/artis.js
@@ -3,5 +3,5 @@ export default function ({ attr, artis, rule, def }) {
     // 血牛钟离，其余词缀权重不高于41.84，确保小生命命中副词缀最高权重
     return rule('迪希雅-血牛', { hp: 100, atk: 30, cpct: 41, cdmg: 41, recharge: 30 })
   }
-  return def({ hp: 80, atk: 75, cpct: 100, cdmg: 100, dmg: 80, phy: 80, recharge: 75 })
+  return def({ hp: 75, atk: 75, cpct: 100, cdmg: 100, dmg: 100, phy: 0, recharge: 75 })
 }
