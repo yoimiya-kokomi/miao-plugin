8f15d531f7f8f496fd40f2bffe695621c73718ca
CUZNIL <76582404+CUZNIL@users.noreply.github.com>
2023-03-14 20:35:18 +0800

    Update artis.js
    修改圣遗物评分判定依据

--- resources/meta/character/可莉/artis.js
+++ resources/meta/character/可莉/artis.js
@@ -1,5 +1,5 @@
 export default function ({ attr, weapon, rule, def }) {
-  if (attr.mastery < 50 && attr.cpct * 2 + attr.cdmg > 320) {
+  if ( attr.mastery * 2 < attr.atk ) {
     return rule('可莉-纯火', { atk: 85, cpct: 100, cdmg: 100, dmg: 100, recharge: 55 })
   }
   return def({ atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, recharge: 30 })
