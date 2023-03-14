ca454990ba7ec991f202014230997d937c4c885f
CUZNIL <76582404+CUZNIL@users.noreply.github.com>
2023-03-15 00:47:11 +0800

    修复可莉不开启组队/圣遗物低配时会产生的bug (#518)
    * Update calc_auto.js
    
    添加可莉总伤
    
    * Update calc.js
    
    修改可莉排行依据
    
    * Update artis.js
    
    修改圣遗物评分判定依据
    
    * 修复可莉面板bug
    
    #喵喵设置组队关闭 时会丢失排行
    
    * 修复可莉圣遗物判定bug
    
    当打蒸发且圣遗物比较烂的时候会误判纯火，已修复（蒸发流可莉总伤最优时精通差不多110，比这个还低一般就是纯火了。）

--- resources/meta/character/可莉/artis.js
+++ resources/meta/character/可莉/artis.js
@@ -1,5 +1,5 @@
 export default function ({ attr, weapon, rule, def }) {
-  if ( attr.mastery * 2 < attr.atk ) {
+  if ( attr.mastery < 110 ) {
     return rule('可莉-纯火', { atk: 85, cpct: 100, cdmg: 100, dmg: 100, recharge: 55 })
   }
   return def({ atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, recharge: 30 })
