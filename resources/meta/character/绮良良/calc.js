51b90dd0fa15a3c2dd4705e88bdac96f69c5026e
DokiAlex <57402050+VeritasAlex@users.noreply.github.com>
2023-05-29 02:35:26 +0800

    添加卡维、白术部分图片资源；更新绮良良信息，添加伤害计算。 (#575)
    * 添加绮良良相关图片资源
    
    * 卡维、白术部分图片资源
    
    * 更新绮良良信息，添加伤害计算

---
+++ resources/meta/character/绮良良/calc.js
@@ -0,0 +1,22 @@
+export const details = [{
+    title: '秘法·惊喜特派伤害',
+    talent: 'q',
+    dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
+}, {
+    title: '护盾最大吸收量',
+    talent: 'e',
+    dmg: ({ talent, calc, attr }, { shield }) => shield(talent.e['护盾吸收量上限2'][0] * calc(attr.hp) / 100 + talent.e['护盾吸收量上限2'][1] * 1)
+}]
+
+export const mainAttr = 'atk,hp,cpct,cdmg'
+
+export const buffs =[{
+    title: '绮良良被动：基于绮良良的生命上限，秘法·惊喜特派伤害提升[qDmg]%',
+    data: {
+        qDmg: ({ attr, calc }) => calc(attr.hp) / 1000 * 0.3
+    }
+}]
+
+
+
+
