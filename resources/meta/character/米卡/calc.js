ff5eadcba398098b524f0bfb38a184b5ae56a099
terry.huang <T@140627>
2023-07-24 11:34:11 +0800

    增加米卡面板治疗量计算与展示

---
+++ resources/meta/character/米卡/calc.js
@@ -0,0 +1,13 @@
+export const details = [ {
+  title: 'Q施放治疗量',
+  dmg: ({ talent, calc, attr }, { heal }) =>
+      heal(talent.q['施放治疗量2'][0] * calc(attr.hp) / 100 + talent.q['施放治疗量2'][1])
+}, {
+  title: '鹰翎治疗量',
+  dmg: ({ talent, calc, attr }, { heal }) =>
+      heal(talent.q['鹰翎治疗量2'][0] * calc(attr.hp) / 100 + talent.q['鹰翎治疗量2'][1])
+}]
+
+export const mainAttr = 'recharge,hp,cpct,cdmg'
+
+export const buffs = []
