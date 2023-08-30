5577e5316c157d02a70fcc48befeb8163d6c4367
Kokomi <102026640+yoimiya-kokomi@users.noreply.github.com>
2023-08-31 04:03:09 +0800

    增加星铁1.3的角色数据与资源

--- resources/meta/info/index.js
+++ resources/meta/info/index.js
@@ -4,22 +4,22 @@ export * from './pool.js'
 export const chestInfo = {
   common: {
     title: '普通宝箱',
-    max: 2701
+    max: 2767
   },
   exquisite: {
     title: '精致宝箱',
-    max: 1654
+    max: 1839
   },
   precious: {
     title: '珍贵宝箱',
-    max: 511
+    max: 566
   },
   luxurious: {
     title: '豪华宝箱',
-    max: 196
+    max: 218
   },
   magic: {
     title: '奇馈宝箱',
-    max: 161
+    max: 186
   }
 }
