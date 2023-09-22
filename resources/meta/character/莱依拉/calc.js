b23c94bf6f7a70daff6bbed8f7867c06e1941559
Aluxes <568976222@qq.com>
2023-09-23 01:36:21 +0800

    fix部分角色护盾计算

--- resources/meta/character/莱依拉/calc.js
+++ resources/meta/character/莱依拉/calc.js
@@ -1,10 +1,12 @@
 export const details = [{
   title: '满层E护盾量',
-  dmg: ({ attr, calc, talent }, { shield }) =>
-    shield(calc(attr.hp) * talent.e['护盾基础吸收量2'][0] / 100 + talent.e['护盾基础吸收量2'][1])
+  dmg: ({ attr, calc, talent, cons }, { shield }) => {
+    const sheildPlus = cons >= 1 ? 1.2 : 1
+    return shield((calc(attr.hp) * talent.e['护盾基础吸收量2'][0] / 100 + talent.e['护盾基础吸收量2'][1]) * sheildPlus)
+  }
 }, {
   title: 'E飞星单伤害',
-  dmg: ({ attr, talent }, dmg) => dmg(talent.e['飞星伤害'], 'e')
+  dmg: ({ talent }, dmg) => dmg(talent.e['飞星伤害'], 'e')
 }, {
   title: 'Q星光弹伤害',
   dmg: ({ attr, calc, talent }, { basic }) => basic(calc(attr.hp) * talent.q['星光弹伤害'] / 100)
@@ -16,7 +18,7 @@ export const buffs = [{
   title: '莱依拉1命：安眠帷幕护盾的伤害吸收量提高20%',
   cons: 1,
   data: {
-    shield: 20
+    _shield: 20
   }
 }, {
   title: '莱依拉6命：飞星与星光弹造成的伤害提升40%',
