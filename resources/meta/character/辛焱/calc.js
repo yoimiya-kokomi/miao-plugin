a81c27400a42e2af3560a148cb781d8da9b134af
Aluxes <568976222@qq.com>
2023-10-02 04:56:45 +0800

    fix:辛焱伤害计算

--- resources/meta/character/辛焱/calc.js
+++ resources/meta/character/辛焱/calc.js
@@ -7,9 +7,11 @@ export const details = [{
     shield(talent.e['三级护盾吸收量2'][0] * calc(attr.def) / 100 + talent.e['三级护盾吸收量2'][1] * 1)
 }, {
   title: 'Q释放伤害',
+  params: { phy: true },
   dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q', 'phy')
 }, {
   title: '重击循环伤害',
+  params: { a2: true, phy: true },
   dmg: ({ talent }, dmg) => dmg(talent.a['重击循环伤害'], 'a2', 'phy')
 }]
 
@@ -30,7 +32,15 @@ export const buffs = [{
 }, {
   title: '辛焱4命：E造成伤害使敌人物理抗性降低15%',
   cons: 4,
+  check: ({ params }) => params.phy === true,
   data: {
-    phyKx: 15
+    kx: 15
+  }
+}, {
+  title: '辛焱6命：进行重击时，基于防御力的50%获得[atkPlus]攻击力加成',
+  cons: 6,
+  check: ({ params }) => params.a2 === true,
+  data: {
+    atkPlus: ({ attr, calc }) => calc(attr.def) * 0.5
   }
 }]
