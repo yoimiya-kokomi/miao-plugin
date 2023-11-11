75b86c4011a394b8c8d8dfcc2aad5f58d60e149a
kaguramaisakuya <1657974606@qq.com>
2023-11-11 21:08:59 +0800

    更新4.2上半宝箱；调整部分命座描述显示效果

--- resources/meta-gs/info/index.js
+++ resources/meta-gs/info/index.js
@@ -4,22 +4,22 @@ export * from './pool.js'
 export const chestInfo = {
   common: {
     title: '普通宝箱',
-    max: 2821
+    max: 2867
   },
   exquisite: {
     title: '精致宝箱',
-    max: 1969
+    max: 2076
   },
   precious: {
     title: '珍贵宝箱',
-    max: 598
+    max: 632
   },
   luxurious: {
     title: '华丽宝箱',
-    max: 232
+    max: 242
   },
   magic: {
     title: '奇馈宝箱',
-    max: 206
+    max: 221
   }
 }
