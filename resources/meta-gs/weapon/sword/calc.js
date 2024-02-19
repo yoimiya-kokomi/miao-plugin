eda7d3162e7c4dbc455a7582a92e940eed76a49b
liangshi <1142607614@qq.com>
2024-02-19 18:27:06 +0800

    有乐御簾切

--- resources/meta-gs/weapon/sword/calc.js
+++ resources/meta-gs/weapon/sword/calc.js
@@ -247,6 +247,13 @@ export default function (step, staticStep) {
       refine: {
         hpPct: step(14 * 2)
       }
+    }],
+    有乐御簾切: [staticStep('defPct', 20),{
+      title: '附近的角色在场上造成岩元素伤害后，普通攻击伤害提升[aDmg]%，元素战伤害提升[eDmg]%；',
+      refine: {
+        aDmg: step(16),
+        eDmg: step(24)
+      }
     }]
   }
 }
