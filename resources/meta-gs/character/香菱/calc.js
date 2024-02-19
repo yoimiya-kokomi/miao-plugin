a4374f300b779844f9e278f607e3654695f9762c
zolay <moe@zozo.me>
2024-02-19 11:02:45 +0800

    修复语法错误（建议下次提PR先自己测一遍）

--- resources/meta-gs/character/香菱/calc.js
+++ resources/meta-gs/character/香菱/calc.js
@@ -6,11 +6,11 @@ export const details = [{
   dmg: ({ talent }, dmg) => dmg(talent.e['喷火伤害'], 'e', 'vaporize')
 }, {
   title: '旋火轮单次伤害',
-  let i = 1,
+  params: { isQ: true},
   dmg: ({ talent }, dmg) => dmg(talent.q['旋火轮伤害'], 'q')
 }, {
   title: '旋火轮单次蒸发',
-  let i = 1,
+  params: { isQ: true },
   dmg: ({ talent }, dmg) => dmg(talent.q['旋火轮伤害'], 'q', 'vaporize')
 }]
 
@@ -24,7 +24,7 @@ export const buffs = [{
     kx: 15
   }
 }, {
-  check: i !== 1,
+  check: ({ params }) => !params.isQ,
   title: '香菱6命：旋火轮持续期间获得15%火伤加成',
   cons: 6,
   data: {
