e3da1112204090f972ab8685c60aa859ba5d5572
liangshi <1142607614@qq.com>
2024-04-17 07:23:50 +0000

    update apps/profile/ProfileChange.js.
    Signed-off-by: liangshi <1142607614@qq.com>

--- apps/profile/ProfileChange.js
+++ apps/profile/ProfileChange.js
@@ -127,8 +127,8 @@ const ProfileChange = {
         return true
       }
 
-      // 匹配武器
-      let wRet = /^(?:等?级?([1-9][0-9])?级?)?\s*(?:([1-5一二三四五满])?(?:(精炼?)|(叠影?))?([1-5一二三四五])?)?\s*(?:等?级?([1-9][0-9])?级?)?\s*(.*)$/.exec(txt)
+      // 匹配武器      
+      let wRet = /^(?:等?级?([1-9][0-9])?级?)?\s*(?:([1-5一二三四五满])?(?:(精炼?)|(叠影?))([1-5一二三四五])?)?\s*(?:等?级?([1-9][0-9])?级?)?\s*(.*)$/.exec(txt)
       if (wRet && wRet[7]) {
         let weaponName = lodash.trim(wRet[7])
         let weapon = Weapon.get(weaponName, game, ret.char.game)
