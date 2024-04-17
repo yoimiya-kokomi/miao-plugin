a53f53e2fd6ea96cdbb2fad8c2f3ed777bd4ed47
liangshi <1142607614@qq.com>
2024-04-17 08:59:05 +0000

    -
    Signed-off-by: liangshi <1142607614@qq.com>

--- apps/profile/ProfileChange.js
+++ apps/profile/ProfileChange.js
@@ -127,10 +127,10 @@ const ProfileChange = {
         return true
       }
 
-      // 匹配武器      
+      // 匹配武器
        let wRet = /^(?:等?级?([1-9][0-9])?级?)?\s*(?:([1-5一二三四五满])(精炼?|叠影?)|(精炼?|叠影?)([1-5一二三四五]))?\s*(?:等?级?([1-9][0-9])?级?)?\s*(.*)$/.exec(txt)
       if (wRet && wRet[7]) {
-        let weaponName = lodash.trim(wRet[7]) 
+        let weaponName = lodash.trim(wRet[7])
         let weapon = Weapon.get(weaponName, game, ret.char.game)
         if (weapon || weaponName === '武器' || Weapon.isWeaponSet(weaponName)) {
           let affix = wRet[2] || wRet[5]
