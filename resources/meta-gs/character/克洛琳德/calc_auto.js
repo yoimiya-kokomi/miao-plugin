96e67938c64ecb1c92267afabe56583625a460ee
liangshi <1142607614@qq.com>
2024-05-24 21:08:28 +0800

    克洛琳德组队计算

--- resources/meta-gs/character/克洛琳德/calc_auto.js
+++ resources/meta-gs/character/克洛琳德/calc_auto.js
@@ -1,24 +1,17 @@
 export const details = [{
-    title: 'E后普通攻击',
-    dmg: ({ talent }, dmg) => dmg(talent.e['驰猎伤害2'][0], 'a')
-}, {
     title: 'E后穿透射击',
     params: { blPct: 0.5 },
     dmg: ({ talent }, dmg) => dmg(talent.e['驰猎伤害2'][1], 'a')
 }, {
-    title: 'E剑击伤害',
-    params: { blPct: 0 },
-    dmg: ({ talent }, dmg) => dmg(talent.e['贯夜伤害2'][0], 'a')
-}, {
-    title: 'E强化剑击伤害',
+    title: 'E后穿透射击激化',
     params: { blPct: 0.5 },
-    dmg: ({ talent }, dmg) => dmg(talent.e['贯夜伤害2'][1], 'a')
+    dmg: ({ talent }, dmg) => dmg(talent.e['驰猎伤害2'][1], 'a', 'aggravate')
 }, {
     title: 'E贯夜·契令伤害',
     dmg: ({ talent }, dmg) => dmg(talent.e['贯夜伤害2'][2], 'a')
 }, {
-    title: 'Q单段伤害',
-    dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害2'][0], 'q')
+     title: 'E贯夜·契令激化',
+     dmg: ({ talent }, dmg) => dmg(talent.e['贯夜伤害2'][2], 'a', 'aggravate')
 }, {
     title: 'Q完整伤害',
     dmg: ({ talent }, dmg) => {
@@ -26,6 +19,29 @@ export const details = [{
     return {
       dmg: q1.dmg * 5,
       avg: q1.avg * 5
+    }
+  }
+}, {
+    title: '克皇 AAAE一轮激化',
+    params: { team: true,  blPct: 0.5 },
+    dmg: ({ talent }, dmg) => {
+    let a1 = dmg(talent.e['驰猎伤害2'][1], 'a')
+    let a2 = dmg(talent.e['驰猎伤害2'][1], 'a', 'aggravate')
+    let e1 = dmg(talent.e['贯夜伤害2'][2], 'a', 'aggravate')
+    return {
+      dmg: a1.dmg * 2 + a2.dmg + e1.dmg,
+      avg: a1.avg * 2 + a2.avg + e1.avg
+    }
+  }
+}, {
+    title: '克皇 Q完整激化',
+    params: { team: true },
+    dmg: ({ talent }, dmg) => {
+    let q1 = dmg(talent.q['技能伤害2'][0], 'q')
+    let q2 = dmg(talent.q['技能伤害2'][0], 'q', 'aggravate')
+    return {
+      dmg: q1.dmg * 3 + q2.dmg * 2,
+      avg: q1.avg * 3 + q2.avg * 2
     }
   }
 }]
@@ -59,6 +75,30 @@ export const buffs = [{
       cpct: 10,
       cdmg: 70
     }
+}, {
+  check: ({ cons, params }) => (cons >= 2 && params.team === true),
+  title: '千夜2命纳西妲：增加精通[mastery],减防[enemyDef]%',
+  data: {
+    mastery: 40,
+    enemyDef: 30
+  }
+}, {
+  check: ({ cons, params }) => (cons < 2 && params.team === true),
+  title: '千夜0命纳西妲：增加精通[mastery]',
+  data: {
+    mastery: 40
+  }
+}, {
+  check: ({ params }) => params.team === true,
+  sort: 7,
+  title: '纳西妲-净善摄受明论：Q范围内在场角色增加精通[mastery]',
+  data: {
+    mastery: 250
+  }
+}, {
+   check: ({ params }) => params.team === true,
+   title: '钟离：降低敌人[kx]%全抗',
+   data: {
+     kx: 20
+   }
 }]
-
-export const createdBy = 'liangshi'
