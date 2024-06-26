d374d86c2bbe5587b403baf1ba5a3618298d4acf
Ca(HCO₃)₂ <143723419+kaguramaisakuya@users.noreply.github.com>
2024-06-26 23:36:40 +0800

    更新4.7宝箱 (#752)

--- resources/meta-gs/info/index.js
+++ resources/meta-gs/info/index.js
@@ -4,22 +4,22 @@ export * from './pool.js'
 export const chestInfo = {
   common: {
     title: '普通宝箱',
-    max: 2980
+    max: 3000
   },
   exquisite: {
     title: '精致宝箱',
-    max: 2221
+    max: 2322
   },
   precious: {
     title: '珍贵宝箱',
-    max: 683
+    max: 705
   },
   luxurious: {
     title: '华丽宝箱',
-    max: 259
+    max: 268
   },
   magic: {
     title: '奇馈宝箱',
-    max: 241
+    max: 251
   }
 }
