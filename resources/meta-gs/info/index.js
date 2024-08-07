64074df4e17a8dc76d1cb8441a768abaab89879f
kaguramaisakuya <1657974606@qq.com>
2024-08-07 23:34:30 +0800

    更新原神4.8宝箱

--- resources/meta-gs/info/index.js
+++ resources/meta-gs/info/index.js
@@ -4,19 +4,19 @@ export * from './pool.js'
 export const chestInfo = {
   common: {
     title: '普通宝箱',
-    max: 3000
+    max: 3059
   },
   exquisite: {
     title: '精致宝箱',
-    max: 2322
+    max: 2383
   },
   precious: {
     title: '珍贵宝箱',
-    max: 705
+    max: 729
   },
   luxurious: {
     title: '华丽宝箱',
-    max: 268
+    max: 275
   },
   magic: {
     title: '奇馈宝箱',
