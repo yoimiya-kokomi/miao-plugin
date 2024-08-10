a0b175ad2d641d3b9b382a5f6eef6f869afcc9c6
jkl8753 <78137354+jkl8753@users.noreply.github.com>
2024-08-10 09:12:01 +0800

    添加妮露在满命条件下的圣遗物自定义评分权重
    妮露在满命条件下，双暴权重更改为100。

---
+++ resources/meta-gs/character/妮露/artis.js
@@ -0,0 +1,6 @@
+export default function ({ cons, rule, def }) {
+    if (cons === 6) {
+      return rule('妮露-满命', { hp: 100, atk: 0, def: 0, cpct: 100, cdmg: 100, mastery: 80, dmg: 100, phy: 0, recharge: 30, heal: 0 })
+    }
+    return def({ hp: 100, atk: 0, def: 0, cpct: 30, cdmg: 30, mastery: 80, dmg: 100, phy: 0, recharge: 30, heal: 0 })
+  }
