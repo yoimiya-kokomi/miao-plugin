e08b91cfa2fc862a60beca7f75644c4b11d7f086
jkl8753 <78137354+jkl8753@users.noreply.github.com>
2024-08-10 11:38:11 +0800

    添加艾梅莉埃、妮露（自定义-满命）的圣遗物评分标准，更改白术的圣遗物评分标准 (#778)
    * 添加艾梅莉埃的圣遗物评分权重
    
    特别的，经测试，艾梅莉埃和克洛琳德一样，攻击力收益极大（可以带攻击头），故艾梅莉埃的攻击权重设置为100。
    
    * 添加妮露在满命条件下的圣遗物自定义评分权重
    
    妮露在满命条件下，双暴权重更改为100。
    
    * 更改白术圣遗物的评分标准
    
    充能从75提升至100，精通从75降为50。

--- resources/meta-gs/artifact/artis-mark.js
+++ resources/meta-gs/artifact/artis-mark.js
@@ -68,7 +68,7 @@ export const usefulAttr = {
   艾尔海森: { hp: 0, atk: 55, def: 0, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, phy: 0, recharge: 35, heal: 0 },
   迪希雅: { hp: 75, atk: 75, def: 0, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, phy: 0, recharge: 55, heal: 0 },
   米卡: { hp: 75, atk: 55, def: 0, cpct: 50, cdmg: 50, mastery: 0, dmg: 75, phy: 75, recharge: 55, heal: 100 },
-  白术: { hp: 100, atk: 0, def: 0, cpct: 30, cdmg: 30, mastery: 75, dmg: 80, phy: 0, recharge: 75, heal: 100 },
+  白术: { hp: 100, atk: 0, def: 0, cpct: 30, cdmg: 30, mastery: 50, dmg: 80, phy: 0, recharge: 100, heal: 100 },
   卡维: { hp: 0, atk: 75, def: 0, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, phy: 0, recharge: 75, heal: 0 },
   绮良良: { hp: 100, atk: 50, def: 0, cpct: 50, cdmg: 50, mastery: 0, dmg: 80, phy: 0, recharge: 0, heal: 0 },
   林尼: { hp: 0, atk: 75, def: 0, cpct: 100, cdmg: 100, mastery: 0, dmg: 100, phy: 0, recharge: 55, heal: 0 },
@@ -86,5 +86,6 @@ export const usefulAttr = {
   阿蕾奇诺: { hp: 0, atk: 75, def: 0, cpct: 100, cdmg: 100, mastery: 75, dmg: 100, phy: 0, recharge: 55, heal: 0 },
   赛索斯: { hp: 0, atk: 30, def: 0, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, phy: 0, recharge: 55, heal: 0 },
   克洛琳德: { hp: 0, atk: 100, def: 0, cpct: 100, cdmg: 100, mastery: 30, dmg: 100, phy: 0, recharge: 35, heal: 0 },
-  希格雯: { hp: 100, atk: 0, def: 0, cpct: 100, cdmg: 100, mastery: 0, dmg: 100, phy: 0, recharge: 30, heal: 100 }
+  希格雯: { hp: 100, atk: 0, def: 0, cpct: 100, cdmg: 100, mastery: 0, dmg: 100, phy: 0, recharge: 30, heal: 100 },
+  艾梅莉埃: { hp: 0, atk: 100, def: 0, cpct: 100, cdmg: 100, mastery: 30, dmg: 100, phy: 0, recharge: 55, heal: 0 }
 }
