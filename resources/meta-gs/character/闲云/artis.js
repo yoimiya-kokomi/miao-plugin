4557a9b6ac405a19da26f6e8a2e52f77b79b2baa
jkl8753 <78137354+jkl8753@users.noreply.github.com>
2024-08-31 07:20:06 +0800

    对闲云满命条件下的评分规则，进行代码优化
    对闲云满命条件下的评分规则，进行代码优化

--- resources/meta-gs/character/闲云/artis.js
+++ resources/meta-gs/character/闲云/artis.js
@@ -1,6 +1,8 @@
+import { usefulAttr } from "../../artifact/artis-mark.js"
+
 export default function ({ cons, rule, def }) {
     if (cons === 6) {
       return rule('闲云-满命', { hp: 0, atk: 100, def: 0, cpct: 100, cdmg: 100, mastery: 0, dmg: 100, phy: 0, recharge: 35, heal: 95 })
     }
-    return def({ hp: 0, atk: 100, def: 0, cpct: 50, cdmg: 50, mastery: 0, dmg: 80, phy: 0, recharge: 75, heal: 95 })
+    return def(usefulAttr['闲云'])
   }
