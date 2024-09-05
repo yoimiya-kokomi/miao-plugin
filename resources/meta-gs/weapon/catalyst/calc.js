1d59803fc2d3f55bc83c398a4ce9a1791a52d86b
其实雨很好 <2122840028@qq.com>
2024-09-05 08:24:47 +0800

    删个逗号

--- resources/meta-gs/weapon/catalyst/calc.js
+++ resources/meta-gs/weapon/catalyst/calc.js
@@ -206,6 +206,6 @@ export default function (step, staticStep) {
       data: {
         aDmg: ({ attr, calc, refine }) => Math.min(Math.floor(calc(attr.hp) / 1000) * step(0.6, 0.1)[refine], step(16)[refine])
       }
-    },
+    }
   }
 }
