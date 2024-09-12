f2c18ff7096cffd4d34123e1c12c1065294c2b0b
jkl8753 <78137354+jkl8753@users.noreply.github.com>
2024-09-12 13:53:35 +0800

    增加椒丘在满命情况下的评分权重
    与0命相比，双暴权重从0 提升到 100，攻击权重从 50 提升至 75.

---
+++ resources/meta-sr/character/椒丘/artis.js
@@ -0,0 +1,8 @@
+import { usefulAttr } from "../../artifact/artis-mark.js"
+
+export default function ({ cons, rule, def }) {
+  if (cons === 6) {
+    return rule('椒丘-满命', { hp: 75, atk: 75, def: 75, speed: 100, cpct: 100, cdmg: 100, stance: 0, heal: 0, recharge: 0, effPct: 100, effDef: 50, dmg: 0 })
+  }
+  return def(usefulAttr['椒丘'])
+}
