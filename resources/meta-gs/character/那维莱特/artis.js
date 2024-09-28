7df716fe308498e801f7b8e829d69271fbef9019
曉K <108290923+SmallK111407@users.noreply.github.com>
2024-09-28 12:35:10 +0800

    Update artis.js

--- resources/meta-gs/character/那维莱特/artis.js
+++ resources/meta-gs/character/那维莱特/artis.js
@@ -1,15 +1,8 @@
 import { usefulAttr } from "../../artifact/artis-mark.js"
 
 export default function ({ attr, weapon, rule, def }) {
-  let title = []
-  let recharge = 55
   if (weapon.name === '万世流涌大典') {
-    title.push('万世流涌大典')
-    recharge = 40
-  }
-  if (title.length > 0) {
-    let role = usefulAttr['那维莱特'];
-    return rule(`那维莱特-${title.join('')}`, { role.hp, role.atk, role.def, role.cpct, role.cdmg, role.mastery, role.dmg, role.phy, recharge, role.heal })
+    return rule(`那维莱特-专武`, { hp: 100, atk: 0, def: 0, cpct: 100, cdmg: 100, mastery: 0, dmg: 100, phy: 0, recharge: 40, heal: 0 })
   }
   return def(usefulAttr['那维莱特'])
 }
