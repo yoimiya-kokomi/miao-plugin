0fd1189fd9bf11a68678561aa6d084001453b4c0
bujidao <107128577+babanbang@users.noreply.github.com>
2024-10-10 21:15:53 +0800

    无法更新希诺宁面板 (#810)

--- resources/meta-gs/character/希诺宁/artis.js
+++ resources/meta-gs/character/希诺宁/artis.js
@@ -1,8 +1,8 @@
 import { usefulAttr } from "../../artifact/artis-mark.js"
 
-export default function ({ weapon, rule, def }) {
+export default function ({ attr, weapon, rule, def }) {
     let title = []
-    let particularAttr = JSON.parse(JSON.stringify(usefulAttr['希诺宁']))
+    let particularAttr = { ...usefulAttr['希诺宁'] }
     if (weapon.name === '西风剑') {
         title.push('西风剑')
         particularAttr.cpct = 100
