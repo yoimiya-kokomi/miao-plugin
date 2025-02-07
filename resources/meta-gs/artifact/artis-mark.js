557eb8204ac5a059e5c8a35ae6f700e6b7121474
冰翼 <78137354+jkl8753@users.noreply.github.com>
2025-02-07 18:41:15 +0800

    添加梦见月瑞希的圣遗物评分权重
    添加梦见月瑞希的圣遗物评分权重

--- resources/meta-gs/artifact/artis-mark.js
+++ resources/meta-gs/artifact/artis-mark.js
@@ -96,5 +96,6 @@ export const usefulAttr = {
   欧洛伦: { hp: 0, atk: 75, def: 0, cpct: 100, cdmg: 100, mastery: 50, dmg: 100, phy: 0, recharge: 75, heal: 0 },
   玛薇卡: { hp: 0, atk: 75, def: 0, cpct: 100, cdmg: 100, mastery: 85, dmg: 100, phy: 0, recharge: 0, heal: 0 },
   茜特菈莉: { hp: 0, atk: 30, def: 0, cpct: 30, cdmg: 30, mastery: 100, dmg: 80, phy: 0, recharge: 100, heal: 0 },
-  蓝砚: { hp: 0, atk: 100, def: 0, cpct: 50, cdmg: 50, mastery: 30, dmg: 80, phy: 0, recharge: 75, heal: 0 }
+  蓝砚: { hp: 0, atk: 100, def: 0, cpct: 50, cdmg: 50, mastery: 30, dmg: 80, phy: 0, recharge: 75, heal: 0 },
+  梦见月瑞希: { hp: 0, atk: 50, def: 0, cpct: 50, cdmg: 50, mastery: 100, dmg: 80, phy: 0, recharge: 45, heal: 95 }
 }
