a1743ee52fb37aab8f9804c7dde021b6c2c60150
kvcfdd <184524253+kvcfdd@users.noreply.github.com>
2025-06-05 00:02:48 +0800

    修复mys面板忆灵技能处理

--- models/serv/api/MysPanelHSRData.js
+++ models/serv/api/MysPanelHSRData.js
@@ -15,7 +15,12 @@ let MysPanelHSRData = {
       level: ds.level,
       cons: ds.rank,
       weapon: ds.equip ? MysPanelHSRData.getWeapon(ds.equip) : null,
-      talent: MysPanelHSRData.getTalent(char, ds.rank, ds.skills),
+      talent: MysPanelHSRData.getTalent(
+        char,
+        ds.rank,
+        ds.skills,
+        ds.servant_detail?.servant_skills || []
+      ),
       trees: MysPanelHSRData.getTrees(ds.skills),
       artis: MysPanelHSRData.getArtifact([...ds.relics, ...ds.ornaments])
     }, 'mysPanelHSR')
@@ -30,23 +35,30 @@ let MysPanelHSRData = {
     }
   },
 
-  getTalent (char, cons, ds = {}) {
-    // 照抄 EnkaData 实现
+  getTalent (char, cons, ds = {}, servant_skills = []) {
     let { talentId = {}, talentCons = {} } = char.meta
     let idx = 0
     let ret = {}
+    const skillKeys = ['a', 'e', 'q', 't', 'z', 'me', 'mt']
     lodash.forEach(ds, (talent_data) => {
       const id = talent_data.point_id
       const lv = talent_data.level
       let key
       if (talentId[id]) {
-        let key = talentId[id]
+        key = talentId[id]
         ret[key] = lv
-      } else if (talent_data.point_type == 2) { // 1 属性加成；2 aeqtz；3 额外能力
-        key = ['a', 'e', 'q', 't', 'z', 'me', 'mt'][idx++]
+      } else if (talent_data.point_type == 2) {
+        key = skillKeys[idx] || `unk${idx}`
+        idx++
         ret[key] = ret[key] || lv
       }
     })
+    if (Array.isArray(servant_skills) && servant_skills.length !== 0) {
+      let me = servant_skills[0]?.level ?? 0
+      let mt = servant_skills[1]?.level ?? 0
+      ret['me'] = me
+      ret['mt'] = mt
+    }
     if (cons >= 3) {
       lodash.forEach(talentCons, (lv, key) => {
         let addTalent = { a: 1, e: 2, q: 2, t: 2, me: 1, mt: 1 }
