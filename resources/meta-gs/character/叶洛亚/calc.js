8ea4e7380fae7dbb6633d41bfe1c1665f5b7b3cd
jkl8753 <crh_27@qq.com>
2026-02-01 11:05:45 +0800

    feat添加叶洛亚伤害计算；调整1命以上基尼奇的圣遗物评分权重。
    基尼奇1命以上时，攻击权重从 85 提升至 100

---
+++ resources/meta-gs/character/叶洛亚/calc.js
@@ -0,0 +1,43 @@
+export const details = [
+  {
+    title: 'E点按伤害',
+    dmg: ({ attr, calc, talent }, { basic }) => basic((calc(attr.mastery) * talent.e['点按伤害2'][0] + calc(attr.def) * talent.e['点按伤害2'][1]) / 100, 'e')
+  },
+  {
+    title: 'E长按伤害',
+    dmg: ({ attr, calc, talent }, { basic }) => basic((calc(attr.mastery) * talent.e['长按伤害2'][0] + calc(attr.def) * talent.e['长按伤害2'][1]) / 100, 'e')
+  },
+  {
+    title: 'Q技能伤害',
+    dmg: ({ attr, calc, talent }, { basic }) => basic((calc(attr.mastery) * talent.q['技能伤害2'][0] + calc(attr.def) * talent.q['技能伤害2'][1]) / 100, 'q')
+  },
+  {
+    title: '(3名水/岩角色)岩元素伤害增加',
+    dmg: ({ attr, calc, talent }) => {
+      return {
+        avg: calc(attr.mastery) * talent.q['岩元素伤害增加'] / 100 + 24
+      }
+    }
+  },
+  {
+    title: '(3名水/岩角色)月结晶反应伤害增加',
+    dmg: ({ attr, calc, talent }) => {
+      return {
+        avg: calc(attr.mastery) * talent.q['月结晶反应伤害增加'] / 100 + 160
+      }
+    }
+  },
+  {
+    title: '2命阿咚额外伤害',
+    cons: 2,
+    dmg: ({ attr, calc }, { basic }) => basic((calc(attr.mastery) * 400 + calc(attr.def) * 200) / 100, 'q')
+  }
+]
+
+export const defDmgIdx = 4
+export const mainAttr = 'def,cpct,cdmg,mastery'
+export const defParams = { Moonsign: 2 }
+
+export const buffs = []
+
+export const createdBy = '冰翼'
