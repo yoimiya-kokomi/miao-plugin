aa90dc86d1190c4d10c83a31b8b7558bbfbd4458
lcroof <lcroof@qq.com>
2026-04-05 20:09:23 +0800

    补充光锥

--- resources/meta-sr/weapon/欢愉/calc.js
+++ resources/meta-sr/weapon/欢愉/calc.js
@@ -20,6 +20,28 @@ export default function (staticIdx, keyIdx) {
     ],
     今日好手气: [
       staticIdx(1, 'cpct')
+    ],
+    菇菇嘎嘎历险记: [
+      staticIdx(1, 'joy'),
+      keyIdx('敌方全体受到的欢愉伤害提高[xeEnemydmg]%', 'xeEnemydmg', 2)
+    ],
+    '未来，有我们一起': [
+      staticIdx(1, 'cdmg'),
+      keyIdx('施放终结技后，使我方全体欢愉度提高[joyPct]%', 'joyPct', 2)
+    ],
+    欢愉满溢祝福: [
+      staticIdx(1, 'atkPct')
+    ],
+    欢迎来到银河城: [
+      staticIdx(1, 'speedPct'),
+      keyIdx('造成的欢愉伤害无视目标[xeIgnore]%的防御力。装备者对自身单体施放终结技时，获得[punchline]点【笑点】', {
+        xeIgnore: 2, punchline: 3
+      })
+    ],
+    邂逅于下一个花季: [
+      staticIdx(1, 'cdmg'),
+      staticIdx(2, 'recharge'),
+      keyIdx('施放欢愉技时，使敌方目标受到的伤害提高[enemydmg]%', 'enemydmg', 3)
     ]
   }
 }
