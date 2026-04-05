98cfa01e9998b263b6a0e46fe58ba942e46ffbc1
lcroof <lcroof@qq.com>
2026-04-05 19:26:32 +0800

    新增火花爻光伤害计算面板

--- resources/meta-sr/weapon/欢愉/calc.js
+++ resources/meta-sr/weapon/欢愉/calc.js
@@ -2,10 +2,21 @@ export default function (staticIdx, keyIdx) {
   return {
     花花世界迷人眼: [
       staticIdx(1, 'cdmg'),
-      keyIdx('无视目标[ignore]%的防御力', 'ignore', 2)
+      (tables) => {
+        return {
+          title: '无视对方防御力[ignore]%，欢愉度提升[joyPct]%',
+          data: {
+            ignore: tables[2] * 4,
+            joyPct: tables[3]
+          }
+        }
+      }
     ],
     当她决定看见: [
-      staticIdx(1, 'speedPct')
+      staticIdx(1, 'speedPct'),
+      keyIdx('【上上签】增加[cpct]%暴击率,增加[cdmg]%暴击伤害', {
+        cpct: 2, cdmg: 3
+      })
     ],
     今日好手气: [
       staticIdx(1, 'cpct')
