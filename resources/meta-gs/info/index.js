f1d849af3184563573d4ae067782d1054ecba315
kaguramaisakuya <1657974606@qq.com>
2026-04-16 23:40:29 +0800

    更新6.5宝箱

--- resources/meta-gs/info/index.js
+++ resources/meta-gs/info/index.js
@@ -4,22 +4,22 @@ export * from './pool.js'
 export const chestInfo = {
   common: {
     title: '普通宝箱',
-    max: 3570
+    max: 3646
   },
   exquisite: {
     title: '精致宝箱',
-    max: 3043
+    max: 3136
   },
   precious: {
     title: '珍贵宝箱',
-    max: 960
+    max: 992
   },
   luxurious: {
     title: '华丽宝箱',
-    max: 366
+    max: 377
   },
   magic: {
     title: '奇馈宝箱',
-    max: 366
+    max: 381
   }
 }
