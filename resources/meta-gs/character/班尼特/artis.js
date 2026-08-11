348bb086714bf6379048c429e617c40bdae85375
冰翼 <78137354+jkl8753@users.noreply.github.com>
2026-08-11 22:20:06 +0800

    Update artis.js

--- resources/meta-gs/character/班尼特/artis.js
+++ resources/meta-gs/character/班尼特/artis.js
@@ -3,26 +3,13 @@ import { usefulAttr } from "../../artifact/artis-mark.js"
 export default function ({ attr, artis, rule, def }) {
   let title = []
   let particularAttr = { ...usefulAttr['班尼特'] }
-  if (attr.recharge >= 250 && ((!artis.is('heal', 5) && attr.hp < 28000) || (artis.is('heal', 5) && attr.hp < 23000))) {
-    title.push('高充能')
-    particularAttr.hp = 0
-    particularAttr.atk = 0
-    particularAttr.def = 0
-    particularAttr.cpct = 0
-    particularAttr.cdmg = 0
-    particularAttr.mastery = 0
-    particularAttr.dmg = 0
-    particularAttr.phy = 0
-    particularAttr.recharge = 100
-    particularAttr.heal = 0
-  }
   if (attr.cpct * 2 + attr.cdmg > 180) {
     title.push('输出')
-    particularAttr.atk = 80
+    particularAttr.atk = 100
     particularAttr.cpct = 100
     particularAttr.cdmg = 100
     particularAttr.dmg = 100
-    particularAttr.recharge = 30
+    particularAttr.recharge = 75
   }
   if (title.length > 0) {
     return rule(`班尼特-${title.join('')}`, particularAttr)
